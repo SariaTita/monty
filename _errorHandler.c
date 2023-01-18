51dabf4ec8a0d62752136b11666d566579f0a7f1
SariaTita <Sariaabbas12345@gmail.com>
2023-01-18 15:12:34 -0800

    -

---
+++ _errorHandler.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+/**
+* _errorHandler - error managment
+*
+* @prmErrorCode: error code
+*/
+void _errorHandler(int prmErrorCode)
+{
+void (*func)();
+func = _getErrorFunction(prmErrorCode);
+if (func != NULL)
+func();
+_freeAppData();
+exit(EXIT_FAILURE);
+}
